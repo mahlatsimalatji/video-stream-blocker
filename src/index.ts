ada72eb427d0a7d0f2bb53ef98c4bc4ae4893921
Mahlatsi Malatji <mahlatsi@andilesolutions.com>
2022-08-08 21:58:52 +0200

    mongoose package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {userStreamRouter} from './routes/userStreamRoute';
+
+import mongoose from 'mongoose';
+
 
 const app = express()
 app.use(json())
+
+app.use(userStreamRouter)
+
+// Connecting to local MongoDB
+mongoose.connect('mongodb+srv://admin:EfcxUwEJj4iR4SI9@mahlatsicluster0.vyhk7xy.mongodb.net/?retryWrites=true&w=majority',
+     () => {
+        console.log('connected to database')
+    })
 
 // Creating server to listen at localhost 3000
 app.listen(3000, () => {
@@ -10,8 +22,3 @@ app.listen(3000, () => {
     console.log("listening to server 3000")
 })
 
-//interface representing userstream db documenr
-interface IUserStreams {
-    username: string;
-    count: number;
-  }
