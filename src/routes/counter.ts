cb04dc0691bc8ff48cb7fa6b6c2d7cf61af05467
Mahlatsi Malatji <mahlatsi@andilesolutions.com>
2022-08-08 21:27:10 +0200

    check initial user stream

--- src/routes/counter.ts
+++ src/routes/counter.ts
@@ -1,8 +1,24 @@
 import express,{Request,Response} from 'express';
+import {UserStreamCount} from "../models/streamCount";
+
 
 const router = express.Router();
 
-router.get('/:username', [], (req:Request,res:Response) => {
+router.get('/:username', [], async (req:Request,res:Response) => {
+    let user = req.params.username
+
+    let userStreamCount = await UserStreamCount.findOne({username: user})
+
+    if(userStreamCount == null){
+        let newUserStream = new UserStreamCount({
+            username: user,
+            count: 1
+        })
+
+        await newUserStream.save()
+
+        res.status(201).send(newUserStream)
+    }
     return res.send('')
 })
 
