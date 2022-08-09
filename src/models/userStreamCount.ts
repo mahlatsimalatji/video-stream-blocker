892ff570610eb6a7ec724e871f5dd9f221ffc60e
Mahlatsi Malatji <mahlatsi@andilesolutions.com>
2022-08-09 09:13:46 +0200

    extra commenting on model file

--- src/models/userStreamCount.ts
+++ src/models/userStreamCount.ts
@@ -6,11 +6,13 @@ interface IUserStreamCount {
     count: number;
   }
 
+// mongoose schema
 const userStreamCountSchema = new mongoose.Schema<IUserStreamCount>({
     username: { type: String, required: [true, 'Username required'] },
     count: { type: Number, required: [true, 'Stream count required'] },
 })
 
+//mongoose model
 const UserStreamCount = mongoose.model<IUserStreamCount>("userStreamCount",userStreamCountSchema)
 
 export {UserStreamCount}
